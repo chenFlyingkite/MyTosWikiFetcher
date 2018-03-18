44384a27ffad5583460c80df7aa7bad58f59431c
chenFlyingkite <30795482+chenFlyingkite@users.noreply.github.com>
2018-03-19 01:50:40 +0800

    Add calculation of  exp curve best fertilizer Level

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,6 @@
 package main;
 
 import main.fetcher.TosWikiCardFetcher;
-import main.fetcher.TosWikiIconFetcher;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import util.logging.L;
@@ -18,6 +17,8 @@ public class Main {
         //TosWikiIconFetcher.me.run();
         TosWikiCardFetcher.me.run();
         //google();
+        //print();
+        //evalExpCurve();
         TicTac.tac("Main ended");
         long tac = System.currentTimeMillis();
         L.log("time = %s", StringUtil.MMSSFFF(tac - tic));
@@ -36,6 +37,44 @@ public class Main {
             System.out.println("r = " + r.body());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void print() {
+
+    }
+
+    private static void evalExpCurve() {
+        int expCurve = 150_0000;
+        int minScExp = 600;
+        int dExp = 1800;
+
+        int max = 100;
+        int[] accuExp = new int[max];
+        int[] scfyExp = new int[max];
+
+        int last = -1, tall = -1;
+        for (int i = 1; i < max; i++) {
+            double dx = 1.0F * (i - 1) / 98;
+            accuExp[i] = (int) Math.ceil(expCurve * dx * dx);
+            scfyExp[i] = minScExp + dExp * (i - 1);
+
+            if (accuExp[i] <= scfyExp[i]) {
+                last = i;
+            }
+
+            if (accuExp[i] - accuExp[i - 1] <= dExp) {
+                tall = i;
+            }
+        }
+
+        System.out.println("tall = " + tall);
+        System.out.println("last = " + last);
+
+        boolean logTable = false;
+        for (int i = 1; i < max && logTable; i++) {
+            String s = String.format("%2s : %7s, %7s", i, accuExp[i], scfyExp[i]);
+            System.out.println(s);
         }
     }
 }
