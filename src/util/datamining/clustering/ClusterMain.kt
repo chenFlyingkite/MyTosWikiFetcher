041a9e493a0f3b057f6d1a0d48bdab07217fa9cf
eric_chen <eric_chen@cyberlink.com>
2018-06-29 21:17:59 +0800

    Add the Clustering By Synchronization, implemented by myself

---
+++ src/util/datamining/clustering/ClusterMain.kt
@@ -0,0 +1,60 @@
+package util.datamining.clustering
+
+import util.datamining.clustering.algo.ClusteringBySynchronization
+import util.datamining.clustering.util.Clusterable
+import util.logging.L
+
+object ClusterMain {
+
+
+    fun main(args : Array<String>) {
+        val dataset = get_1D()
+        L.log("set = $dataset")
+
+        val cs = ClusteringBySynchronization<Data>()
+        cs.setDataSet(dataset)
+        cs.run()
+    }
+
+    private fun get_1D(): List<Data> {
+        val list = ArrayList<Data>()
+        val s = arrayOf(1, 5, 3, 4, 2, 9, 10)
+        for (i in s) {
+        //for (i in 1..10) {
+            val d = Data()
+            d.x = i * 1.0
+            list.add(d)
+        }
+        return list
+    }
+
+    class Data : Clusterable<Data> {
+        override fun toString(): String {
+            return "$x"
+        }
+        var x = 0.0
+        override fun distance(y: Data): Double {
+            return Math.abs(x - y.x)
+        }
+
+        override fun copy(): Data {
+            val d = Data()
+            d.x = x
+            return d
+        }
+
+        override fun getDimension(): Int {
+            return 1
+        }
+
+        override fun getXi(i: Int): Double {
+            return x
+        }
+
+        override fun setXi(i: Int, xi: Double) {
+            x = xi
+        }
+
+    }
+
+}
---
+++ src/util/datamining/clustering/util/PngMaker.kt
@@ -0,0 +1,64 @@
+package util.datamining.clustering.util
+
+import util.files.FileUtil
+import util.tool.TicTac2
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+
+object PngMaker {
+    private val mClock = TicTac2()
+    val colors = arrayOf(
+            Color(255,   0,   0)
+            , Color(  0, 255,   0)
+            , Color(  0,   0, 255)
+            , Color(255, 255,   0)
+            , Color(255,   0, 255)
+            , Color(  0, 255, 255)
+            , Color(128,   0,   0)
+            , Color(  0, 128,   0)
+            , Color(  0,   0, 128)
+            , Color(128, 128,   0)
+    )
+
+    fun getColorModed(i: Int): Color {
+        return colors[i % colors.size]
+    }
+
+    fun loadImage(f: File): BufferedImage? {
+        // Step : Load image by ImageIO
+        var img: BufferedImage? = null
+        try {
+            img = ImageIO.read(f)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return img
+    }
+
+    fun _png(name: String): String {
+        if (!name.endsWith(".png")) {
+            return "$name.png"
+        }
+        return name
+    }
+
+    fun into(img: BufferedImage?, file: File) {
+        FileUtil.ensureDelete(file)
+        FileUtil.createFile(file)
+
+        //mClock.tic()
+        // Step : Write to output
+        try {
+
+            ImageIO.write(img, "png", file)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        //mClock.tac("ImageIO write >> " + file.absolutePath)
+    }
+}
