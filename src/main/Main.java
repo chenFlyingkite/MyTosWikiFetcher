e1a57cc754a4d1f86c490f3701895fc002c7a82e
eric_chen <eric_chen@cyberlink.com>
2018-07-06 19:51:19 +0800

    Update image links

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,7 @@ package main;
 import main.fetcher.TosActiveSkillFetcher;
 import main.fetcher.TosAmeSkillFetcher;
 import main.fetcher.TosWikiCardFetcher;
-import main.fetcher.TosWikiFilePeeker;
 import main.fetcher.TosWikiHomeFetcher;
-import util.datamining.clustering.ClusterMain;
 import util.logging.L;
 import util.tool.StringUtil;
 import util.tool.TicTac;
@@ -15,7 +13,7 @@ public class Main {
         long tic = System.currentTimeMillis();
         TicTac.tic();
         //-- Regular
-        if (false) {
+        if (true) {
             TosWikiHomeFetcher.me.run();
             TosWikiCardFetcher.me.run();
             TosAmeSkillFetcher.me.run();
@@ -26,12 +24,12 @@ public class Main {
         //TosPageArchiveFetcher.me.run();
         //TosWikiIconFetcher.me.run();
         //TosWikiSummonerLevelFetcher.me.run();
-        TosWikiFilePeeker.me.run();
+        //TosWikiFilePeeker.me.run();
         //TosWikiImageFileFetcher.me.run();
         //MobileComm.run();
         //ASD.run();
         //TosWikiStageFetcher.me.run();
-        ClusterMain.INSTANCE.main(args);
+        //ClusterMain.INSTANCE.main(args);
         //Statistics.run();
         TicTac.tac("Main ended");
         long tac = System.currentTimeMillis();
