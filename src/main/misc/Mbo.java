c207233707820bd5ebfe13d694b738976f116ec9
chenFlyingkite <30795482+chenFlyingkite@users.noreply.github.com>
2022-01-07 16:50:10 +0800

    add mbo

---
+++ src/main/misc/Mbo.java
@@ -0,0 +1,282 @@
+package main.misc;
+
+public class Mbo {
+    /*
+    Planning Confirmed by Executor: Eric Chen(2021/1/11  12:16)
+    Planning Confirmed by Manager: Todd Chen(2021/1/26  13:57)
+    Evaluation Confirmed by Executor: Eric Chen(2021/7/16  16:58)
+    Evaluation Confirmed by Manager: Todd Chen(2021/7/21  12:07)
+1.	Objective	DueDate	Weight%(A)	Achievement %(B)	Eval. Comment	Score
+            (A*B)
+    Engaged in development of project to add features and fix bugs
+
+    Photo Director - iOS	2021/6/30	40 %	93%	Good	37.2
+            93%	Mainly focus on main features of March & April, Light Hit & Surreal Art
+    Also self-study GPUImage and shader languages
+
+    Item	Key Result	Evaluation
+    Comment	Due
+    Date	Weight
+%	Achieve
+    ment%
+            1. Getting familiar with
+1-1> the structure
+1-2> framework of The Project
+2. Creating good structure for better maintenance	Fixing the eBugs and adding feature to ensure The Project follows the schedule on time
+    Simply describe the developing monuments.	Very good	2021/6/30	25%	100%
+
+    Implemented features and items (Order by time)
+    Overall
+    Organize FLUtility family and extend capability
+    Fix misc UI/UX issues and preformance issues
+
+2021/01
+    Add Edit page each page's Call-To-Action when user using premium items
+    Light hit 1st version capability study (Swift)
+    Self-study GPUImage framework, and study shade language code for core of image processing
+
+2021/02
+    Light Hit implement/adjustment to fit spec , includes querying server content, memory issues
+
+2021/03
+    Correct Light Hit crashlytics
+    Surreal art 1st version capability development
+
+2021/04
+    Implement Surreal Art details to fit spec and correct performance & memory issues
+    Implement CMSAPI for PHD iOS, and make surreal art use the CMSAPI
+
+2021/05
+    Opening tutorial version 2 recreated, and refector & implement the new mechanism
+2021/05/w3 start to work from home
+    Update opening tutorial videos/ strings
+    Add splash birdview
+
+2021/06
+    Android : Implemennt Setting page IAP big banner
+    iOS : Implemennt Setting page IAP big ba	100%
+            1. Fixing eBugs of The Project
+1-1> with quality
+1-2> and time efficiency.	Fixing the eBugs of The Project with quality, time efficiency and no severe side effect so as to ensure The Project follows the schedule on time	Good	2021/6/30	35%	80%
+    see 1-1	80%
+            1. Adding the required features
+1-1> with quality
+1-2> and efficiency	Fix the critical/blocking ebugs and enhance user experience of the new added features	Very good	2021/6/30	40%	100%
+    see 1-1	100%
+
+
+            2.	Objective	DueDate	Weight%(A)	Achievement %(B)	Eval. Comment	Score
+            (A*B)
+    Dynamically Support other project development for those in need.
+
+            ActionDirector - Android	2021/6/30	30 %	90%	Good	27
+            90%	Mainly support for PhotoDirector Android, and as consult for ActionDirector Android
+
+    Item	Key Result	Evaluation
+    Comment	Due
+    Date	Weight
+%	Achieve
+    ment%
+    Getting familiar with the overall structure of the project supporting.	Fixing the eBugs and adding feature to ensure supported project follows the schedule on time
+    Simply describe the developing monuments.	Good	2021/6/30	25%	80%
+
+    Implemented features and items (Order by time)
+    Overall
+    Organize FLUtility family and extend capability
+    Fix misc UI/UX issues and preformance issues
+
+2021/01
+    Add Edit page each page's Call-To-Action when user using premium items
+    Light hit 1st version capability study (Swift)
+    Self-study GPUImage framework, and study shade language code for core of image processing
+
+2021/02
+    Light Hit implement/adjustment to fit spec , includes querying server content, memory issues
+
+2021/03
+    Correct Light Hit crashlytics
+    Surreal art 1st version capability development
+
+2021/04
+    Implement Surreal Art details to fit spec and correct performance & memory issues
+    Implement CMSAPI for PHD iOS, and make surreal art use the CMSAPI
+
+2021/05
+    Opening tutorial version 2 recreated, and refector & implement the new mechanism
+2021/05/w3 start to work from home
+    Update opening tutorial videos/ strings
+    Add splash birdview
+
+2021/06
+    Android : Implemennt Setting page IAP big banner
+    iOS : Implemennt Setting page IAP big ba	80%
+    Fixing the eBugs or performing some unit test if the project requires or demands.	Fixing eBugs with quality and no side effect if any eBugs is fixed.	Very good	2021/6/30	50%	100%
+    see 2-1	100%
+    Adding new feature if the project requires or demands.	Adding new feature with quality and time efficiency if new feature is added.
+    Enhance the user experience on the new/existing features	Good	2021/6/30	25%	80%
+    see 2-1	80%
+
+
+            3.	Objective	DueDate	Weight%(A)	Achievement %(B)	Eval. Comment	Score
+            (A*B)
+    Study / training technical related materials	2021/6/30	10 %	60%	OK	6
+            100%	Provide the recharge program
+
+    Item	Key Result	Evaluation
+    Comment	Due
+    Date	Weight
+%	Achieve
+    ment%
+    Study either books, developer website or other materials	Share organized content by creating wiki page / PPT, provide production-ready codes to SVN or sending emails to coworkers or team members	OK	2021/6/30	100%	60%
+    Eric Chen: UX Program - 2021/05/13
+    PPT
+    Video : P:\PhotoDirector_Touch\Recharge\UX Program_EricChen\[Recharge] UX Program - Eric Chen 2021-05_2021-05-13_18-37-51.mp4	100%
+
+
+            4.	Objective	DueDate	Weight%(A)	Achievement %(B)	Eval. Comment	Score
+            (A*B)
+    Improvement of every project communication with main RDs, Managers, PMs, Arts, Server team, and other related staff.	2021/6/30	20 %	88%	Good	17.6
+            88%	Discuss with android / ios team members and provide solution / idea
+
+    Item	Key Result	Evaluation
+    Comment	Due
+    Date	Weight
+%	Achieve
+    ment%
+            1. Receive confirmation on items assigned from coworkers / new self-dedication
+1>1 new features
+1>2 Bug fixes and expected results
+1>3 Request with priority with upper managers.	Let coworkers receive efficient, effective, well-organized content includes
+1. Schedules and planing
+2. currect self tasks and concurrent staff tasks
+3. Mainly goal and expected behaviors
+4. Code quality	Good	2021/6/30	40%	80%
+    Be familiar with Java, Kotlin, Objective C, and Swift syntax
+80%
+        1. Provide synchonized communication on items assigned from coworkers / new self-dedication
+1>1 new features
+1>2 Bug fixes and expected results
+1>3 Request with priority with upper managers.	Let coworkers receive efficient, effective, well-organized content includes
+1. Schedules and planing
+2. currect self tasks and concurrent staff tasks
+3. Mainly goal and expected behaviors
+4. Code quality	Good	2021/6/30	40%	100%
+    Discuss with team members with issue & solutions and provide mechanism easy to maintain and prevents possible pitfalls
+100%
+        1. Provide self-expression and opinion if new UX / UI idea when communications	Let coworkers receive efficient, effective, well-organized content includes
+1. Core soul of idea
+2. Abstract Implementation way
+3. Schedule prediction
+4. Estimatable results	Good	2021/6/30	20%	80%
+    Add comment and documents for main engines and those core part	80%
+
+
+            5.	Objective	DueDate	Weight%(A)	Achievement %(B)	Eval. Comment	Score
+            (A*B)
+    Keep self be good in Computer Science basic and English ability.	2021/6/30	0 %	80%	OK	0
+            80%	The capability is in good state and still nee to take care health state both with CS basics
+
+    Item	Key Result	Evaluation
+    Comment	Due
+    Date	Weight
+%	Achieve
+    ment%
+    In this year, make myself always keep good in CS foundations and advanced skills	Using online materials to evaluate myself.	OK	2021/6/30	40%	80%
+            0624LC597,237,311,49
+            0712LC619,249,321,49	80%
+    In this year, make myself in good English speaking / discussion and aim at discussing code / implementation ideas in full English (like talk with RDs uses only English) with no obstacles and no misunderstandings.	Using online materials to evaluate myself.	OK	2021/6/30	30%	80%
+    Spend too less time on self-training and figuring out solutions	80%
+    In this year, keep myself in fast to provide the concrete implementation codes with well-visual commentted and designing ideas with good discussions	Using online materials to evaluate myself.
+    And commit concreate contents to svn	OK	2021/6/30	30%	80%
+    Need to have a more intensive and effective training. Still need to have more better periodical training & life	80%
+
+
+            6.	Objective	DueDate	Weight%(A)	Achievement %(B)	Eval. Comment	Score
+            (A*B)
+    Being able to handle different co-working RDs / managers personality and provide good impact on working efficiency on work.	2021/6/30	0 %	100%	OK	0
+            100%	Acceptable
+
+    Item	Key Result	Evaluation
+    Comment	Due
+    Date	Weight
+%	Achieve
+    ment%
+    Make myself to be able to fit into teams by keeping my characteristics and team styles, even if the team is not my ideal team.	Be fast / self-motivated to suit into teams even if team manager is not assigning tasks and give ambiguous navigation and guidance on projects.
+    Decrease personal discussion, understanding of staff if staff and I still failed to find a good way / protocols of negotiation, keep in mind that keep personal and company affairs separated.
+    OK	2021/6/30	40%	100%
+            .	100%
+    Consider to propose transfer team request by myself if self and team fails to keep balancing.
+            (for item 1)	No mixing personal affairs and workplace affairs into complicated situations.
+    Keep works on, no personal emotions, judgements and any unfair items on staffs to have bad impact on work.
+            OK	2021/6/30	30%	100%
+            .	100%
+    Consider to Do As Romans Do if new team have special characteristics and keep self in ideal state (for item 2)	Contribute usable items and good investigations for work and staff communications.
+    Even if myself is in a difficult environments of team and members, still self-motivated to find / research new materials and have concrete result by leaving as code or comments.
+            OK	2021/6/30	30%	100%
+            .	100%
+
+
+    Total Score : 87.8
+    Contributions/Improvements
+1. Major contributions or improvements for the past 6 months :
+    Reviewed Staff	Reviewing Manager
+1.	2021/01
+    Add Edit page each page's Call-To-Action when user using premium items
+    Light hit 1st version capability study (Swift)
+    Self-study GPUImage framework, and study shade language code for core of image processing
+2.	2021/02
+    Light Hit implement/adjustment to fit spec , includes querying server content, memory issues
+3.	2021/03
+    Correct Light Hit crashlytics
+    Surreal art 1st version capability development
+4.	2021/04
+    Implement Surreal Art details to fit spec and correct performance & memory issues
+    Implement CMSAPI for PHD iOS, and make surreal art use the CMSAPI
+5.	2021/05
+    Opening tutorial version 2 recreated, and refector & implement the new mechanism
+2021/05/w3 start to work from home
+    Update opening tutorial videos/ strings
+    Add splash birdview
+6.	2021/06
+    Android : Implemennt Setting page IAP big banner
+    iOS : Implemennt Setting page IAP big banner
+    Android : Add the send feedback of user subscribe state
+7.	2021/07
+    Android : Implemennt ShutterStock dlc version 2 (to extend after current items)
+    FaceMeSDK Android : Implement 1st page of login meeting
+    FaceMeSDK iOS : Implement 1st page of login meeting
+8.	Overall
+    Organize FLUtility family and extend capability
+    Fix misc UI/UX issues and preformance issues
+2. Items requiring more improvements over the past 6 months:
+    Reviewed Staff	Reviewing Manager
+1.	Improvement of every project communication with main RDs, Managers, PMs, Arts, Server team, and other related staff.
+            2.	Take care health state of self, including rest, exercising, recreation and working.
+3.	Keep self be good in Computer Science basic and English ability.
+Comment :
+
+1. Good contribution on implementing two major features of PHDI, LightHits and Surreal Art. Both of them are required solid understanding on GPUImage framework. You were aggressive and were working very hard for this technical challenge to deliver them on time.
+
+2. Please consider to do more practices to clearly explain the architecture of your code to other members. It would be very helpful to the technical discussion and you can learn more ideas from other members.
+
+3. Let's take care your health state and keep positive attitude on every challenge.
+
+4. Your second part of recharge for UX is not completed yet and you did not book time to do it. This is bad.
+
+5. Thank you for your open mind to support FaceMe Fintech project. It is also your show time to show that you can develop both Android project and iOS project. This is amazing!
+
+( by Todd Chen 2021/7/21  12:07)
+Fill in evaluation comment
+
+( by Eric Chen 2021/7/16  16:58)
+Main objectives:
+1. PhD-iOS new feature development with good coding architecture and with good quality.
+2. Dynamically support PhD-iOS UX/MX tasks.
+3. Share Android/iOS programming experience to help other members to grow.
+
+( by Todd Chen 2021/1/26  13:57)
+1st version
+
+( by Eric Chen 2021/1/11  12:16)
+        */
+}
